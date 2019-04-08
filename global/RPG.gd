756c452d3871be8aa0401b0b8188a96cfc18aa21
CyberFilth <ch22011978@gmail.com>
2019-04-08 17:12:07 +0100

    Played with UI sidebar

--- global/RPG.gd
+++ global/RPG.gd
@@ -16,7 +16,7 @@ const LAYER_FX = 3
 
 var TORCH_RADIUS = 6
 
-var MAP_SIZE = Vector2(120,80)
+var MAP_SIZE = Vector2(100, 80) # Changed from 120, 80
 var MAX_ROOMS = 60
 var ROOM_MIN_SIZE = 5
 var ROOM_MAX_SIZE = 13
