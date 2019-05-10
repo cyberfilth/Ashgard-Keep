10833c251d37a25276701d17abccf6535652470e
ch22011978@gmail.com <ch22011978@gmail.com>
2019-05-10 12:21:27 +0100

    CONTINUE option disabled if no save file found

--- scenes/TitleMenu/TitleMenu.gd
+++ scenes/TitleMenu/TitleMenu.gd
@@ -5,6 +5,13 @@ onready var versionlabel = get_node('Menu/Version')
 func _ready():
 	randomize()
 	versionlabel.set_text(GameData.version)
+	# check for save file
+	var file = File.new()
+	var save_exists = file.file_exists(GameData.SAVEGAME_PATH)
+	var button = get_node("Menu/CentreRow/Buttons/Continue")
+	button.set_disabled(!save_exists)
+	if !save_exists:
+		get_node('Menu/CentreRow/Buttons/Continue/Label').add_color_override("font_color", Color(0, 0.55, 0.55, 1))
 	
 
 
