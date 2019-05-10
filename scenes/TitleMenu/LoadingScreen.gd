2473a1f987110735a77903f4dbcf4a9f762d4810
ch22011978@gmail.com <ch22011978@gmail.com>
2019-05-10 12:27:48 +0100

    Added CONTINUE screen

--- scenes/TitleMenu/LoadingScreen.gd
+++ scenes/TitleMenu/LoadingScreen.gd
@@ -3,13 +3,5 @@ extends Control
 onready var versionlabel = get_node('Menu/Version')
 
 func _ready():
-	versionlabel.set_text(GameData.version)
-	# Timer added to give time for screen to load
-	var t = Timer.new()
-	t.set_wait_time(1)
-	t.set_one_shot(true)
-	self.add_child(t)
-	t.start()
-	yield(t, "timeout")
-	t.queue_free()
+	GameData.restore_game = true
 	get_tree().change_scene('res://scenes/Game/Game.tscn')
