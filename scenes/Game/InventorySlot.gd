3ad821387b30456730a4c372206dab0c42afe429
ch22011978@gmail.com <ch22011978@gmail.com>
2019-05-17 15:28:31 +0100

    Merging code before working on Inventory fix

--- scenes/Game/InventorySlot.gd
+++ scenes/Game/InventorySlot.gd
@@ -6,6 +6,7 @@ func add_contents(what):
 	contents.append(what)
 	what.item.inventory_slot = self
 	update_slot()
+	
 
 func remove_contents(what):
 	contents.remove(contents.find(what))
@@ -22,6 +23,7 @@ func update_slot():
 		get_node('Icon').set_texture(null)
 		get_node('Brand').set_texture(null)
 		set_disabled(true)
+
 	var count = contents.size()
 	var txt = str(count) if count > 1 else ''
 	get_node('Count').set_text(txt)
@@ -31,10 +33,5 @@ func update_slot():
 func _ready():
 	connect("mouse_enter", get_parent(), "_on_slot_mouse_enter", [self])
 	connect("mouse_exit", get_parent(), "_on_slot_mouse_exit")
-
-
-func _on_InventorySlot_pressed():
-	var obj = contents[0]
-	contents.remove(0)
-	obj.item.use()
-	update_slot()
+	connect("pressed", get_parent(), "_on_slot_button_pressed", [self])
+	connect("pressed", get_parent(), "_on_slot_item_used", [self])
