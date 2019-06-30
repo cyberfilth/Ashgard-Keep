f4f9f318c595e3b14812a97c51d17ce0793d9f56
cyberfilth <ch22011978@gmail.com>
2019-06-30 15:18:10 +0100

    Improved level up screen with dividers

--- scenes/Game/LevelUp.gd
+++ scenes/Game/LevelUp.gd
@@ -1,9 +1,13 @@
 extends PopupPanel
 
-onready var levellabel = get_parent().get_node('LevelUp/VBoxContainer/Label')
+onready var levellabel = get_parent().get_node('LevelUp/VBoxContainer/LevelUp')
+onready var flavourlabel = get_parent().get_node('LevelUp/VBoxContainer/FlavourText')
 
 func _ready():
 	pass
 
 func start(level):
 	levellabel.set_text("You have advanced to " + str(level))
+	flavourlabel.set_text("Your experiences in the Keep have sharpened your skills.\nYou can choose to increase one of the following abilities.")
+# offer choice of 20% HP increase or add level to ATT or DEF
+# increase HP before exiting
