75a49d4afb691d7b126de39f27576dd157e7ba9b
cyberfilth <ch22011978@gmail.com>
2019-07-01 14:16:55 +0100

    Options added to level up screen

--- scenes/Game/LevelUp.gd
+++ scenes/Game/LevelUp.gd
@@ -2,6 +2,9 @@ extends PopupPanel
 
 onready var levellabel = get_parent().get_node('LevelUp/VBoxContainer/LevelUp')
 onready var flavourlabel = get_parent().get_node('LevelUp/VBoxContainer/FlavourText')
+onready var increaseATT_label = get_parent().get_node('LevelUp/VBoxContainer/IncreaseATT')
+onready var increaseDEF_label = get_parent().get_node('LevelUp/VBoxContainer/IncreaseDEF')
+onready var increaseATTDEF_label = get_parent().get_node('LevelUp/VBoxContainer/IncreaseATTDEF')
 
 func _ready():
 	pass
@@ -9,5 +12,6 @@ func _ready():
 func start(level):
 	levellabel.set_text("You have advanced to " + str(level))
 	flavourlabel.set_text("Your experiences in the Keep have sharpened your skills.\nYou can choose to increase one of the following abilities.")
-# offer choice of 20% HP increase or add level to ATT or DEF
-# increase HP before exiting
+	increaseATT_label.set_text("Increase Attack by "+str(level))
+	increaseDEF_label.set_text("Increase Defence by "+str(level))
+	increaseATTDEF_label.set_text("Increase both Attack and Defence by "+str(floor(level/2)))
