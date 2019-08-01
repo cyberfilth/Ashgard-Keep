a8cb79212cf9f7998b0e9c7bbdfc55a39f242f77
cyberfilth <ch22011978@gmail.com>
2019-08-01 23:20:54 +0100

    Slime dungeon added

--- global/DungeonThemes.gd
+++ global/DungeonThemes.gd
@@ -4,13 +4,15 @@ extends Node
 const WALL_SANDSTONE = [0, 9]
 const WALL_BRICK_DARK = [20, 26]
 const WALL_STONE_GREY = [34, 37]
-const WALL_ASCII = [66,66]
+const WALL_ASCII = [66, 66]
+const WALL_SLIME_STONE = [68, 71]
 
 const FLOOR_SANDSTONE = [10, 19]
 const FLOOR_BRICK_DARK = [27, 33]
 const FLOOR_COBBLE_BLOOD = [38, 49]
 const FLOOR_GREEN_SLABS = [50, 65]
 const FLOOR_ASCII = [67,67]
+const FLOOR_BOG_GREEN = [72, 75]
 
 # Darkness families
 var DARKNESS1 = Color( 0.27, 0.27, 0.27, 1 )
@@ -18,16 +20,18 @@ var DARKNESS2 = Color( 0.75, 0.59, 0.46, 1 )
 var DARKNESS3 = Color( 0.25, 0.32, 0.34, 1 )
 var DARKNESS4 = Color( 0.25, 0.32, 0.34, 1 )
 var DARKNESS5 = Color( 0.25, 0.32, 0.34, 1 )
+var DARKNESS6 = Color( 0.25, 0.32, 0.34, 1 )
 
 # Dungeon tile families
 var FAMILY1 = [FLOOR_SANDSTONE, WALL_SANDSTONE]
 var FAMILY2 = [FLOOR_BRICK_DARK, WALL_BRICK_DARK]
 var FAMILY3 = [FLOOR_COBBLE_BLOOD, WALL_STONE_GREY]
 var FAMILY4 = [FLOOR_GREEN_SLABS, WALL_STONE_GREY]
-var FAMILY5 = [FLOOR_ASCII, WALL_ASCII]
+var FAMILY6 = [FLOOR_ASCII, WALL_ASCII]
+var FAMILY5 = [FLOOR_BOG_GREEN, WALL_SLIME_STONE]
 
 # All dungeon theme numbers
-var original_dungeon_themes = [0,1,2,3]
+var original_dungeon_themes = [0,1,2,3,4,5]
 
 # Dungeon theme including tileset and darkness settings
 var dung_themes = [
@@ -47,9 +51,13 @@ darkness = DARKNESS3
 tileset = FAMILY4,
 darkness = DARKNESS4
 },
-{ # 4 - ASCII
+{ # 6 - Slime Green
 tileset = FAMILY5,
 darkness = DARKNESS5
+},
+{ # 5 - ASCII tiles - used for testing only
+tileset = FAMILY6,
+darkness = DARKNESS6
 }
 ]
 
