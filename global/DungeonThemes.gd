4e699ba50fda2e811c3f75ba454781b0495b312b
cyberfilth <ch22011978@gmail.com>
2019-08-01 08:18:45 +0100

    Test ASCII dungeon, just for fun

--- global/DungeonThemes.gd
+++ global/DungeonThemes.gd
@@ -4,23 +4,27 @@ extends Node
 const WALL_SANDSTONE = [0, 9]
 const WALL_BRICK_DARK = [20, 26]
 const WALL_STONE_GREY = [34, 37]
+const WALL_ASCII = [66,66]
 
 const FLOOR_SANDSTONE = [10, 19]
 const FLOOR_BRICK_DARK = [27, 33]
 const FLOOR_COBBLE_BLOOD = [38, 49]
 const FLOOR_GREEN_SLABS = [50, 65]
+const FLOOR_ASCII = [67,67]
 
 # Darkness families
 var DARKNESS1 = Color( 0.27, 0.27, 0.27, 1 )
 var DARKNESS2 = Color( 0.75, 0.59, 0.46, 1 )
 var DARKNESS3 = Color( 0.25, 0.32, 0.34, 1 )
 var DARKNESS4 = Color( 0.25, 0.32, 0.34, 1 )
+var DARKNESS5 = Color( 0.25, 0.32, 0.34, 1 )
 
 # Dungeon tile families
 var FAMILY1 = [FLOOR_SANDSTONE, WALL_SANDSTONE]
 var FAMILY2 = [FLOOR_BRICK_DARK, WALL_BRICK_DARK]
 var FAMILY3 = [FLOOR_COBBLE_BLOOD, WALL_STONE_GREY]
 var FAMILY4 = [FLOOR_GREEN_SLABS, WALL_STONE_GREY]
+var FAMILY5 = [FLOOR_ASCII, WALL_ASCII]
 
 # All dungeon theme numbers
 var original_dungeon_themes = [0,1,2,3]
@@ -42,6 +46,10 @@ darkness = DARKNESS3
 { # 4 - Green flagstones
 tileset = FAMILY4,
 darkness = DARKNESS4
+},
+{ # 4 - ASCII
+tileset = FAMILY5,
+darkness = DARKNESS5
 }
 ]
 
