9507bf6135aab45119ca4eeb37568848975180bf
cyberfilth <ch22011978@gmail.com>
2019-09-25 20:14:55 +0100

    Added level 2 Goblin

--- global/DungeonThemes.gd
+++ global/DungeonThemes.gd
@@ -203,7 +203,7 @@ t_e1 = 'monsters/greenskins/goblin',
 t_e2 = 'monsters/animals/lvl2/scorpion'
 },
 { # Level 2
-t_e1 = 'monsters/greenskins/goblin',
+t_e1 = 'monsters/greenskins/lvl2/goblin',
 t_e2 = 'monsters/animals/lvl2/scorpion'
 },
 { # Level 3
---
+++ objects/monsters/greenskins/lvl2/goblin_AI.gd
@@ -0,0 +1,31 @@
+# goblin_lvl2_AI
+
+extends Node
+
+onready var owner = get_parent()
+onready var new_name = PlotGen.npc_greenskin2
+
+func _ready():
+	owner.ai = self
+	owner.get_node(".").name = new_name
+
+func take_turn():
+	if owner.fighter.has_status_effect('confused'):
+		wander()
+	var target = GameData.player
+	var distance = owner.distance_to(target.get_map_pos())
+	if owner.fighter.hp <= 9 && distance < 2:
+		wander()
+	if distance <= (GameData.player_radius - 2):
+		if distance <= 1:
+			owner.fighter.fight(target)
+		else:
+			owner.step_to(target.get_map_pos())
+
+func wander():
+	var UP = randi()%2
+	var DOWN = randi()%2
+	var LEFT = randi()%2
+	var RIGHT = randi()%2
+	var dir = Vector2( RIGHT-LEFT, DOWN-UP )
+	owner.step(dir)
