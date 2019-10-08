d3c0ad6aafbc963926667f2e7a074cb8711cd153
cyberfilth <ch22011978@gmail.com>
2019-10-08 21:20:17 +0100

    Added seed to Randomise function for debugging

--- scenes/Start.gd
+++ scenes/Start.gd
@@ -1,7 +1,17 @@
 extends Node
 
 # This is kept in a separate scene from the Title Menu
-# so the randomize function is only called once
+# so the randomize function is only called once.
+#
+# Seed is printed to the console whilst debugging to
+# allow the same game to be run
+
+var game_seed = 0
+
 func _ready():
 	randomize()
+	if !game_seed:
+		game_seed = randi()
+	seed(game_seed)
+	print("Seed: ", game_seed) # Printed out for testing
 	get_tree().change_scene('res://scenes/TitleMenu/TitleMenu.tscn')
